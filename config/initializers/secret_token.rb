b000e9f6da24969ee474c7b0b739a9288b5645f3
John Johnson <dampboxdev@gmail.com>
2013-08-13 16:47:01 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2917206225f455a60847b7d1845f9485502b0235e04d4152d40ecac8c60eabcdeecd99ef3cbf50072f9499bf136eb329f370a1c32a81b03869cccb93e89693ee'
